446678a64d6559f56c820a21cf481cbccea6197b
fmoneo <fmoneo@gmail.com>
2013-09-02 23:09:39 -0700

    v1.1 Enhancements & bug fixing
    - Added support for files with /r/n and /n (mac vs. pc).
    - Better error handling. Warning when the file has unknown types.
    - Hours column is now also used for Story Points for User Stories.
    - Added image to main page.
    - Cleaned up the code.

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,13 +1,17 @@
+// Global Variables
+$("#myTable").tablesorter();
+var jsonOut = [];
+
+// Add events to buttons
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
+document.getElementById('getPDFButton').addEventListener('click', getPDFClick, false);
+
 // Check for the various File API support.
 if (window.File && window.FileReader && window.FileList && window.Blob) {
-  // Great success! All the File APIs are supported.
+	// Great success! All the File APIs are supported.
 } else {
-  alert('The FileReader() API are not fully supported in this browser.');
+	alert('The FileReader() API are not fully supported in this browser.');
 }
-
-$("#myTable").tablesorter();
-
-var jsonOut = [];
 
 function getPDFClick() {
 	$.ajax({  
@@ -17,88 +21,79 @@ function getPDFClick() {
 		contentType: "application/json",  
 		data: JSON.stringify(jsonOut),  
 		success: function(data){              
-			//alert("success :-)");
-			//console.log(data);
 			var win=window.open("/pdf/out.pdf");
 			win.focus();
 		},  
 		error: function(){  
-			alert("fail :-(");  
+			$("#genMsg").append("<span class='glyphicon glyphicon-remove'></span>&nbsp;PDF Generator Failed!!!");
+			$("#genMsg").removeClass("hidden").removeClass("alert-success"); 
+			$("#genMsg").addClass('alert-danger');  
 		}  
 	}); 
 }
 
 function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
 
-    // files is a FileList of File objects. List some properties.
-    var output = [];	
+	// Reset all elements on the page
+	$("#genMsg").empty();
+	$("#genMsg").addClass("hidden").addClass("alert-success").removeClass("alert-danger").removeClass("alert-warning"); ;
+	jsonOut.length = 0;
+	$('#myTable > tbody').empty();	
+	$("#getPDFButton").addClass("disabled");
+	
+	var numOfUnknownTypes = 0;
+
+	var files = evt.target.files; // FileList object
+
+	// files is a FileList of File objects. List some properties.
+	var output = [];	
 	
 	var reader = new FileReader();
 	
-    for (var i = 0, f; f = files[i]; i++) {
-		/*output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-                  f.size, ' bytes, last modified: ',
-                  f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-                  '</li>');*/
+	for (var i = 0, f; f = files[i]; i++) {
 				  
 		reader.onloadend = function(evt) {
-			if (evt.target.readyState == FileReader.DONE) { // DONE == 2
-				//document.getElementById('byte_content').textContent = evt.target.result;
+			if (evt.target.readyState == FileReader.DONE) { // DONE == 2				
 				var text = evt.target.result;
 				//break the lines apart
-				var lines = text.split('\r\n');
+				var lines = text.split(/\r\n|\n/);
+				
+				//For each line in the file
 				for(var j = 0; j<lines.length; j++){
 					var scrumCard = new Object();
 					
-					var information = lines[j].split(',');
+					var information = $.csv.toArray(lines[j]);
+					//var information = lines[j].split(',');
 					
-					/*if(information.length != 6){
+					if(information.length != 6 && lines[j].length>0 && j<lines.length-1){
 						$("#genMsg").append("<span class='glyphicon glyphicon-remove'></span>&nbsp;CSV file doesn't have 6 columns");
 						$("#genMsg").removeClass("hidden").removeClass("alert-success"); 
 						$("#genMsg").addClass('alert-danger');
 						return;
-					}*/
+					}
 					
-					scrumCard.id = information[0];
-					scrumCard.title = information[1];
-					scrumCard.assignedTo = information[2];
-					scrumCard.pri = information[3];
-					scrumCard.hours = information[4];
-					scrumCard.type = information[5];
-					
-					
-					$('#myTable > tbody:last').append('<tr style="display: table-row;"><td>'+scrumCard.id+'</td><td>'+scrumCard.title+'</td><td>'+scrumCard.assignedTo+'</td><td>'+scrumCard.pri+'</td><td>'+scrumCard.hours+'</td><td>'+scrumCard.type+'</td></tr>');
-					//console.log('Add');					
-					
-					jsonOut.push(scrumCard);
-					//console.log(j + ">>" + lines[j]);
-				}
-				//console.log(JSON.stringify(jsonOut));
-				
-				//$.post("/ws", JSON.stringify(jsonOut)) 
-				//.done(function(data) {
-				//	alert("Response: " + data);
-				//});
-				
-				/*$.ajax({  
-					url: "/ws",  
-					type: "POST",  
-					dataType: "text",  
-					contentType: "application/json",  
-					data: JSON.stringify(jsonOut),  
-					success: function(data){              
-						//alert("success :-)");
-						console.log(data);
-						//var win=window.open("/pdf/out.pdf");
-						//win.focus();
-					},  
-					error: function(){  
-						$("#genMsg").append("Error!!");
-						$("#genMsg").removeClass("hidden").removeClass("alert-success"); 
-						$("#genMsg").addClass('alert-danger');
-					}  
-				}); */
+					if(information.length == 6){
+						scrumCard.id = information[0];
+						scrumCard.title = information[1];
+						scrumCard.assignedTo = information[2];
+						scrumCard.pri = information[3];
+						scrumCard.hours = information[4];
+						scrumCard.type = information[5];
+						
+						if(scrumCard.type != "User Story" && 
+							scrumCard.type != "Dev Task" &&
+							scrumCard.type != "Test Task" &&
+							scrumCard.type != "PM Task" &&
+							scrumCard.type != "Release Task" &&
+							scrumCard.type != "Bug") {
+							numOfUnknownTypes ++;
+						}
+						
+						$('#myTable > tbody:last').append('<tr style="display: table-row;"><td>'+scrumCard.id+'</td><td>'+scrumCard.title+'</td><td>'+scrumCard.assignedTo+'</td><td>'+scrumCard.pri+'</td><td>'+scrumCard.hours+'</td><td>'+scrumCard.type+'</td></tr>');					
+						
+						jsonOut.push(scrumCard);
+					}
+				} //for each line
 				
 				var resort = true, // re-apply the current sort
 				callback = function(){
@@ -109,22 +104,24 @@ function handleFileSelect(evt) {
 				
 				$("#getPDFButton").removeClass("disabled");
 				
-				$("#genMsg").append("<span class='glyphicon glyphicon-ok'></span>&nbsp;&nbsp;File upload completed successfully");
-				$("#genMsg").removeClass("hidden");
 				
-			}
-		};
+				
+				if(numOfUnknownTypes==0){
+					$("#genMsg").append("<span class='glyphicon glyphicon-ok'></span>&nbsp;&nbsp;File upload completed successfully.");
+					$("#genMsg").removeClass("hidden");
+				} else {
+					$("#genMsg").append("<span class='glyphicon glyphicon-exclamation-sign'></span>&nbsp;&nbsp;File upload completed successfully, but the file has <strong>" + numOfUnknownTypes + " unknown type(s)</strong>. <p><small><em>Please make sure that the Type column has one of these values: User Story, Dev Task, Test Task, PM Task, Release Task or Bug.</em></small></p>");
+					$("#genMsg").removeClass("hidden").removeClass("alert-success").addClass('alert-warning');
+				}
+				
+			} // if file read is done
+		};		
 		
+		reader.readAsBinaryString(f);		
 		
-		reader.readAsBinaryString(f);
-		
-		
-    }
-    //document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+	} // for each file	
 	
-	
-  }
+  } //function handleFileSelect
 
-  document.getElementById('files').addEventListener('change', handleFileSelect, false);
-  document.getElementById('getPDFButton').addEventListener('click', getPDFClick, false);
+
 
